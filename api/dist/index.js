fed97aa7bf62ceb3d479fe435c618c942f64ae4c
Luis Gabriel <janco7249@gmail.com>
2021-11-12 22:28:01 -0400

    new build

--- api/dist/index.js
+++ api/dist/index.js
@@ -1,2 +1,10 @@
 "use strict";
-const hola = "asdasd";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const app_1 = __importDefault(require("./app"));
+require("./database");
+app_1.default.listen(app_1.default.get('port'), () => {
+    console.log('Server on port', app_1.default.get('port'));
+});
