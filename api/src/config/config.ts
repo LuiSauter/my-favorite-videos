74ffbc60579c1700b5b7d2ea80bfcd151316171c
Luis Gabriel <janco7249@gmail.com>
2021-11-12 22:24:10 -0400

    add JWT envinroment variables

--- api/src/config/config.ts
+++ api/src/config/config.ts
@@ -2,8 +2,8 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export default {
-  MONGO_DATABASE: process.env.MONGO_DB_URL || 'somesecrettoken',
+  MONGO_DATABASE: process.env.MONGO_DB_URL || '',
   MONGO_DATABASE_TEST: process.env.MONGO_DB_URL_TEST || '',
   PORT: process.env.PORT || 4000,
-  jwtSecret: process.env.JWT_SECRET || ''
+  jwtSecret: process.env.JWT_SECRET || 'tokentest'
 }
