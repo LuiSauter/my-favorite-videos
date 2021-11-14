5db843c5382213b00d7dfb7e589fcf2e9a0ecdea
Luis Gabriel <janco7249@gmail.com>
2021-11-13 22:22:33 -0400

    style: add styled in video container

---
+++ frontend/src/components/VIdeos/VideoContainer.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components'
+
+const VideoContainer = styled.section`
+  width: 97%;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(270px, 1fr));
+  justify-content: center;
+  margin: 1rem auto;
+  flex-wrap: wrap;
+  gap: 1rem;
+  /* max-width: 500px; */
+  @media screen and (max-width:635px) {
+    width: 100%;
+  }
+`
+
+export default VideoContainer
