49170db1b5307b8d039c0ce13c14421858e3fc27
Luis Gabriel <janco7249@gmail.com>
2021-11-13 22:29:51 -0400

    testing default ⏳

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import { Route, Switch } from 'wouter'
 import reportWebVitals from './reportWebVitals'
+
+import VideoForm from './components/VIdeos/VideoForm/VideoForm'
+import Home from './pages/Home'
+import Nav from './components/Navbar/Nav'
+
+import {ToastContainer} from 'react-toastify'
+
+import { Layout } from './styles/Layout'
+import GlobalStyle from './styles/GlobalStyle'
+import 'react-toastify/dist/ReactToastify.css'
+import { VideoStateProvider } from './context/videos/VIdeoStateProvider'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <VideoStateProvider>
+      <GlobalStyle />
+      <Layout>
+        <Nav />
+        <Switch>
+          <Route path='/' component={Home} />
+          <Route path='/new-video' component={VideoForm} />
+          <Route path='/update/:id' component={VideoForm} />
+        </Switch>
+        <ToastContainer />
+      </Layout>
+    </VideoStateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 )
